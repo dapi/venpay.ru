fcafb29a6fd961b11d06ba11dfc0444be61910fe
Danil Pismenny <danil@brandymint.ru>
2019-12-23 15:28:43 +0300

    Move mobile.css from sprockets to webpack

--- app/javascript/packs/mobile.js
+++ app/javascript/packs/mobile.js
@@ -1,4 +1,6 @@
 require("@rails/ujs").start()
+import 'bootstrap/dist/css/bootstrap'
+import './mobile/qr-scanner.sass'
 import QrScanner from 'qr-scanner/qr-scanner.min.js';
 QrScanner.WORKER_PATH = 'qr_scanner/qr-scanner-worker.min.js'
 window.QrScanner = QrScanner;
