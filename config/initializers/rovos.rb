35c4e9000550fa12841033ffc78539fce765c4d0
Danil Pismenny <danil@brandymint.ru>
2019-12-23 15:04:51 +0300

    Minor change rovos-broker initiation for development

--- config/initializers/rovos.rb
+++ config/initializers/rovos.rb
@@ -2,16 +2,14 @@ require './lib/rovos_client'
 
 class RovosClient
   def self.build
-    if Rails.env.production?
+    if ENV.has_key?('ROVOS_BROKER_HOST')
+      RovosClient.new ENV.fetch('ROVOS_BROKER_HOST')
+    else
       client_cert = OpenSSL::X509::Certificate.new File.read Rails.root.join('config/certs/broker.venpay.ru.pem')
       client_key = OpenSSL::PKey.read File.read Rails.root.join('config/certs/broker.venpay.ru.key')
       RovosClient.new(
         Rails.application.credentials[:rovos_broker_host],
         ssl: { client_cert:  client_cert, client_key: client_key, verify: false }
-      )
-    else
-      RovosClient.new(
-        ENV.fetch('ROVOS_BROKER_HOST'),
       )
     end
   end
