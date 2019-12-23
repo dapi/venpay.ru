c4af1d5eb67b59403bedf1752f7d17b5b06dd095
Danil Pismenny <danil@brandymint.ru>
2019-12-23 15:58:05 +0300

    Don't ask branch for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,26 +3,25 @@
 lock '3.11.2'
 
 set :user, 'wwwuser'
-
 set :repo_url, 'https://github.com/BrandyMint/venpay.ru.git' if ENV['USE_LOCAL_REPO'].nil?
 set :keep_releases, 10
-
 set :linked_files, %w[config/master.key]
 set :linked_dirs, %w[log node_modules tmp/pids tmp/cache tmp/sockets public/qrcodes public/assets public/packs config/certs]
-
 set :config_files, fetch(:linked_files)
-
 set :deploy_to, -> { "/home/#{fetch(:user)}/#{fetch(:application)}" }
 
-if ENV['BRANCH']
-  set :branch, ENV['BRANCH']
+if ENV.has_key? 'BRANCH'
+  if ENV['BRANCH']=='ask'
+    ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+  else
+    set :branch, ENV['BRANCH']
+  end
 else
-  ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+  set :branch, 'master'
 end
 
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
-
 set :nvm_node, File.read('.nvmrc').strip
 set :nvm_map_bins, %w[node npm yarn rake]
 
@@ -35,8 +34,6 @@ set :assets_dependencies,
 
 set :keep_assets, 2
 set :local_assets_dir, 'public'
-
 set :puma_init_active_record, true
-
 set :db_local_clean, false
 set :db_remote_clean, true
