bea6f021760e835c8ced9e526c136bf1f22b38e1
Danil Pismenny <danil@brandymint.ru>
2019-12-24 21:49:31 +0300

    add rails-ujs to mobile.js

--- app/javascript/packs/mobile.js
+++ app/javascript/packs/mobile.js
@@ -1,5 +1,5 @@
+require("@rails/ujs").start()
 import 'bootstrap/dist/css/bootstrap'
-console.log('mobile')
 document.addEventListener("DOMContentLoaded", function(event) {
   // hack auto-hide address bar
   window.scrollTo(0,1);
